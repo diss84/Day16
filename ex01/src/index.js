7daec2740d15da895b2e6ea6e81e0e85f8ffb31a
Mirela <adisserdarevic@gmail.com>
2021-03-23 21:33:14 +0100

    Added file index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,16 +1,16 @@
 // Only change code below this line
 class Car {
     constructor(manufacturer, model, production){
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.production = production;
+        this._manufacturer = manufacturer;
+        this._model = model;
+        this._production = production;
     }
 }
 
 // Only change code above this line
 const bmw = new Car("BMW", "BMW X3", 2003);
-console.log(bmw.manufacturer);
-console.log(bmw.model);
-console.log(bmw.production);
+console.log(bmw._manufacturer);
+console.log(bmw._model);
+console.log(bmw._production);
 
 module.exports = Car;
