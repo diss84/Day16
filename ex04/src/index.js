a68eb7d7c8fb1fcf3d94de213e056c2eb2d9959a
Mirela <adisserdarevic@gmail.com>
2021-03-23 22:06:32 +0100

    Added file index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,51 @@
+class Pets {
+
+    constructor(name, legs) {
+
+      this._name = name;
+
+      this._legs = legs;
+
+    }
+
+    walk() {
+
+        var numberOfLegs = this._name + " is walking on " + this._legs + " legs";
+
+        return numberOfLegs;
+
+    }
+
+}
+
+class Dog extends Pets {
+
+    constructor(name, legs) {
+
+        super(name, legs);
+
+    }
+
+    walk() {
+
+        return super.walk();
+
+    }
+
+    bark() {
+
+        var dogSay = this._name + " says WUF-WUF";
+
+        return dogSay;
+
+    }
+
+}
+
+ 
+
+let dog = new Dog("Pujdo", 4);
+
+console.log(dog.walk());
+
+console.log(dog.bark());
