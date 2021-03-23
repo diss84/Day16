7a7a45a57fa89fbf89588b903970f6f52ee53688
Mirela <adisserdarevic@gmail.com>
2021-03-23 21:59:09 +0100

    Added file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+//Only change code below this line
+const firstName = (firstName) => {
+    return firstName.toUpperCase();
+  };
+  const lastName = (lastName) => {
+    return lastName.toLowerCase();
+  };
+  exports.firstName = firstName;
+  exports.lastName = lastName;
+  console.log(firstName("firstname"));
+  console.log(lastName("LASTNAME"));
+  //Only change code above this line
